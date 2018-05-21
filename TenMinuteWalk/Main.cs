af61b44839f41df7936f997b6a50c08615198533
nad407 <37850036+nad407@users.noreply.github.com>
2018-05-21 17:11:41 +0200

    CodeWars Kata

---
+++ TenMinuteWalk/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kata
+{
+    public class main
+    {
+       public static void Main(string[] args)
+        {
+            bool valid = TenMinuteWalk.IsValidWalk(new string[] { "n", "s", "n", "s", "n", "s", "n", "s", "n", "s" });
+            Console.WriteLine(valid);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TenMinuteWalk/TenMinuteWalk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Kata
+{
+
+    public class TenMinuteWalk 
+    {
+        public TenMinuteWalk()
+        {
+        }
+
+        public static bool IsValidWalk(string[] walk)
+        {
+            if (walk.Length == 10)
+            {
+                int[] arr = new int[] { 0, 0, 0, 0 };
+                for (int i = 0; i < walk.Length; i++)
+                {
+                    if (walk[i].CompareTo("n") == 0) { arr[0]++; }
+                    if (walk[i].CompareTo("s") == 0) { arr[1]++; }
+                    if (walk[i].CompareTo("e") == 0) { arr[2]++; }
+                    if (walk[i].CompareTo("w") == 0) { arr[3]++; }
+                }
+
+                if ((arr[0] == arr[1]) & (arr[2] == arr[3]))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
